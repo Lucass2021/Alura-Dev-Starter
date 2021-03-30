61096c0d2a053c94db49d45c4cdb6728037f7878
Lucas Dias <lucas.allx@hotmail.com>
2021-03-30 07:48:58 -0300

    AluraFlix 2

--- AluraFlix 2/main2.js
+++ AluraFlix 2/main2.js
@@ -1,17 +1,20 @@
 function adicionarFilme() {
     let campoFilme = document.getElementById("filme");
     let filmeFavorito = campoFilme.value;
+    campoFilme = ""
+}
+
+function validaFilme() {
     if (filmeFavorito.endsWith(".jpg")) {
         listarFilmesnaTela(filmeFavorito);
     } else {
         alert("Imagem invalida");
     }
-    campoFilme.value = "";
 }
+
 
 function listarFilmesnaTela(filmeFavorito) {
     let listaFilmes = document.getElementById("listaFilmes");
     let elementoFilme = (`<img src= ${filmeFavorito} </img>`);
     listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme;
 }
-
--- AluraFlix/main2.js
+++ AluraFlix/main2.js
@@ -6,16 +6,21 @@ listaFilmes.push("https://m.media-amazon.com/images/M/MV5BMTEyODg2NzkwMDBeQTJeQW
 listaFilmes.push("https://m.media-amazon.com/images/M/MV5BMTkyODg2MzQwMV5BMl5BanBnXkFtZTgwNTA2MjE1MDI@._V1_UX182_CR0,0,182,268_AL_.jpg");
 listaFilmes.push("https://m.media-amazon.com/images/M/MV5BMTM5MjkwMTI0MV5BMl5BanBnXkFtZTcwODQwMTc0OQ@@._V1_UY268_CR7,0,182,268_AL_.jpg");
 
-3
-let lista = parseInt(prompt("Escolha: 1 para Sons of Anarchy, 2 para Grimm e 3 para Dexter"));
+function myButton() {
+    let lista = parseInt(prompt("Escolha: 1 para Sons of Anarchy, 2 para Grimm e 3 para Dexter"));
 
-if (lista == 1) {
-    document.write(`<img src= ${listaFilmes[0]} </img>`);
-} else if (lista == 2) {
-    document.write(`<img src=${listaFilmes[1]} </img>`);
-} else if (lista == 3) {
-    document.write(`<img src= ${listaFilmes[2]}`);
-} else {
-    alert("Por favor, digite um valor válido");
+    if (lista == 1) {
+        document.write(`<img src= ${listaFilmes[0]} </img>`);
+
+    } else if (lista == 2) {
+        document.write(`<img src=${listaFilmes[1]} </img>`);
+    } else if (lista == 3) {
+        document.write(`<img src= ${listaFilmes[2]}`);
+    } else {
+        alert("Por favor, digite um valor válido");
+    }
 }
+
+
+
 
